d11796aa68c0d82c8b884f001198bcd719eecdb0
ayaz <d.ayaz.extern@optica.de>
2019-07-04 08:23:25 +0300

    Controller and index pages  created.

---
+++ pet-clinic-web/src/main/java/ayaz/com/ayazpetclinic/controllers/OwnerController.java
@@ -0,0 +1,14 @@
+package ayaz.com.ayazpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"/owners","owners/index","owners/index.html"})
+    public String ListOfOwner(){
+        return "owners/index";
+    }
+}
