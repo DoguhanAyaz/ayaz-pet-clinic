bf0f9276ac9e54eb543bc83f08793d58fbab844c
ayaz <d.ayaz.extern@optica.de>
2019-08-26 12:43:42 +0300

    Fixing broken links.

--- pet-clinic-data/src/main/java/com/ayaz/ayazpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/ayaz/ayazpetclinic/model/Vet.java
@@ -1,16 +1,14 @@
 package com.ayaz.ayazpetclinic.model;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 @Entity
 @Table(name = "vets")
 public class Vet extends Person {
 
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "vet")
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialities", joinColumns = @JoinColumn(name = "vet_id"), inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialities = new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
